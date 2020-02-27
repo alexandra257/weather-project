2c0c694cfc83adb32a728b474ae5117ceb14d9d5
Alexandra <alexandrajn17@gmail.com>
2020-02-27 11:07:19 +0000

    fetching data from an external server, parsing JSON into JS object & accessing what we want

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const https = require("https"); //native node https module already bundled with node, so don't need to install
+
+const app = express(); //initialises a new express app
+
+//we must include the strict https://
+app.get("/", function (req, res) {
+    //store the url in a constant so we can work with our get request more easily!
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b31653f586e18698dbe8bcbd66195659&units=metric";
+
+    //fetch the data from an exernal server and get it in JSON format
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data) //parse the JSON into a JS object so we can access the information we want
+            const temp = weatherData.main.temp
+            const description = weatherData.weather[0].description
+            console.log(temp);
+            console.log(description);
+        })
+    })
+    res.send("Server is up and running.");
+
+})
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.");
+})
