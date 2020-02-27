692526b1d435c982bcb9ed44d64f35f5fb81cf8b
Alexandra <alexandrajn17@gmail.com>
2020-02-27 12:03:15 +0000

    can't diagnose 400 error

--- app.js
+++ app.js
@@ -6,36 +6,35 @@ const app = express(); //initialises a new express app
 
 app.use(bodyParser.urlencoded({ extended: true })); //necessary code for us to be able to start parsing through the body of the post request
 //we must include the strict https://
+
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.post("/", function (req, res) {
-    console.log(req.body.cityName);
+    const query = req.body.cityName;
+    const apiKey = "b31653f586e18698dbe8bcbd66195659";
+    const unit = "metric";
+    //store the url in a constant so we can work with our get request more easily!
+    const url = "https://api.openweathermap.org/data/2.5/weather?" + query + "&appid=" + apiKey + "&units=" + unit;
+
+    //fetch the data from an exernal server and get it in JSON format
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data); //parse the JSON into a JS object so we can access the information we want
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "<p>");
+            res.write("<h1>The temperature in" + query + "is " + temp + " degrees Celcius.</h1 > ");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+        });
+    });
 })
-
-// const query = "London";
-// const apiKey = "b31653f586e18698dbe8bcbd66195659";
-// const unit = "metric";
-// //store the url in a constant so we can work with our get request more easily!
-// const url = "https://api.openweathermap.org/data/2.5/weather?" + query + "&appid=" + apiKey + "&units=" + unit;
-
-// //fetch the data from an exernal server and get it in JSON format
-// https.get(url, function (response) {
-//     console.log(response.statusCode);
-
-//     response.on("data", function (data) {
-//         const weatherData = JSON.parse(data) //parse the JSON into a JS object so we can access the information we want
-//         const temp = weatherData.main.temp
-//         const weatherDescription = weatherData.weather[0].description
-//         const icon = weatherData.weather[0].icon
-//         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//         res.write("<p>The weather is currently " + weatherDescription + "<p>");
-//         res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
-//         res.write("<img src=" + imageURL + ">");
-//         res.send()
-//     })
-// })
 
 
 app.listen(3000, function () {
