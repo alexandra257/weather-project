1561174ec383641f0051bfb50214ab2dc1bf0fb2
Alexandra <alexandrajn17@gmail.com>
2020-03-17 22:12:22 +0000

    error fixed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.post("/", function (req, res) {
     const apiKey = "b31653f586e18698dbe8bcbd66195659";
     const unit = "metric";
     //store the url in a constant so we can work with our get request more easily!
-    const url = "https://api.openweathermap.org/data/2.5/weather?" + query + "&appid=" + apiKey + "&units=" + unit;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
     //fetch the data from an exernal server and get it in JSON format
     https.get(url, function (response) {
@@ -24,12 +24,12 @@ app.post("/", function (req, res) {
 
         response.on("data", function (data) {
             const weatherData = JSON.parse(data); //parse the JSON into a JS object so we can access the information we want
-            const temp = weatherData.main.temp;
+            const temp = Math.round(weatherData.main.temp);
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<p>The weather is currently " + weatherDescription + "<p>");
-            res.write("<h1>The temperature in" + query + "is " + temp + " degrees Celcius.</h1 > ");
+            res.write("<h1>The temperature in " + query + " is " + temp + " °C.</h1 > ");
             res.write("<img src=" + imageURL + ">");
             res.send();
         });
